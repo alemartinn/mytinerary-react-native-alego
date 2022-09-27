75849ef21756314ce724d805f6f31fe5c2fccda6
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-27 18:18:12 -0300

    working on new pages

---
+++ src/screens/Cities.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const CitiesScreen = () => {
+    return (  
+        <View>
+            <View>Cities Screen</View>
+            <View>Cities Screen 2</View>
+            <View>Cities Screen 3</View>
+            <View>Cities Screen 4</View>
+            <View>Cities Screen 5</View>
+            <View>Cities Screen 6</View>
+            <View>Cities Screen 7</View>
+            <View>Cities Screen 8 </View>
+            <View>Cities Screen 9</View>
+        </View>
+    );
+}
+ 
+export default CitiesScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { View, Text } from 'react-native'
 
 const HomeScreen = () => {
     return (  
         <View>
-            
+            <View>Home Screen</View>
+            <View>Home Screen 2</View>
+            <View>Home Screen 3</View>
+            <View>Home Screen 4</View>
+            <View>Home Screen 5</View>
+            <View>Home Screen 6</View>
+            <View>Home Screen 7</View>
+            <View>Home Screen 8 </View>
+            <View>Home Screen 9</View>
         </View>
     );
 }
