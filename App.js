806887f0b7ca86c0ff6fea5b0c924514a6f3ee4b
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-27 18:15:28 -0300

    working on app.js

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   return (
-    <Provider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>Hi World</Text>
-        <Text style={styles.text}>MyTinerary</Text>
-        <Button
-          title="See more"
-          color="#c49"
-          style={styles.btnSeeMore}
-          accessibilityLabel='Learn more about this button'
-        />
-        <StatusBar style="auto" />
-    </SafeAreaView>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.text}>Hi World</Text>
+          <Text style={styles.text}>MyTinerary</Text>
+          <Button
+            title="See more"
+            color="#c49"
+            style={styles.btnSeeMore}
+            accessibilityLabel='Learn more about this button'
+            />
+          <StatusBar style="auto"/>
+        </SafeAreaView>
+      </NavigationContainer>
     </Provider>
   );
 }
