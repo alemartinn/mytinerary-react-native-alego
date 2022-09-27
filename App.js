46130ec9d06f5ae9faa4c957fad459b8eb4415c9
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-27 17:12:02 -0300

    redux has been implemented

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Provider } from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>Hi World</Text>
+        <Text style={styles.text}>MyTinerary</Text>
+        <Button
+          title="See more"
+          color="#c49"
+          style={styles.btnSeeMore}
+          accessibilityLabel='Learn more about this button'
+        />
+        <StatusBar style="auto" />
+    </SafeAreaView>
+    </Provider>
   );
 }
 
@@ -17,4 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 50,
+    color: '#777'
+  },
+  btnSeeMore:{
+    color: '#c49'
+  }
 });
