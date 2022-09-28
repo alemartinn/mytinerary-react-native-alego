32e54374f49f8e436ab5b36da0cd04b5769b7c44
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-28 14:44:25 -0300

    removing irrelevants lines

--- src/screens/Cities.js
+++ src/screens/Cities.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native'
+import CityCards from '../components/CityCards';
 
 const CitiesScreen = () => {
     return (  
         <View>
-            <View>Cities Screen</View>
-            <View>Cities Screen 2</View>
-            <View>Cities Screen 3</View>
-            <View>Cities Screen 4</View>
-            <View>Cities Screen 5</View>
-            <View>Cities Screen 6</View>
-            <View>Cities Screen 7</View>
-            <View>Cities Screen 8 </View>
-            <View>Cities Screen 9</View>
+            <CityCards/>
         </View>
     );
 }
