4b12162752a23747d3d227c8462bdd0b6d7b5868
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-29 20:47:19 -0300

    fixing navigator

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import Drawer from './src/NavigationSrc/Drawer';
+import CitiesScreen from './src/screens/CitiesScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import HomeScreen from './src/screens/HomeScreen';
 import store from './store';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Drawer/>
+        <Stack.Navigator initialRouteName="Home">
+          {/* <Drawer/> */}
+          <Stack.Screen name="Drawer" component={Drawer} options={{headerShown: false}}/>
+          <Stack.Screen name="Details" component={DetailsScreen}/>
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
