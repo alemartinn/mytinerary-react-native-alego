6311e3e051ca8389bd9d84266af4dc4564ab38d1
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-29 20:46:31 -0300

    Create screen new city

---
+++ src/screens/NewCityScreen.js
@@ -0,0 +1,110 @@
+import { ImageBackground, View, Text, StyleSheet, Alert } from 'react-native';
+import { useAddNewCityMutation } from '../../features/citiesAPI';
+import { TextInput } from 'react-native-gesture-handler';
+import Button from '../components/Button';
+import{ useState } from 'react'
+
+function NewCityScreen() {
+    const image = require('../../assets//backgroundHero.png')
+    const [city, setCity] = useState(
+        {
+            city: "",
+            country: "",
+            photo: "",
+            population: 1000,
+            foundation: 1000,
+        }
+    )
+    const [addCity] = useAddNewCityMutation()
+    const handleSubmit = async () => {
+        addCity(city)
+        Alert.alert(
+            "You created a new city!",
+            "Now you can see it in City Pages.",
+            [
+              { text: "Ok", onPress: () => console.log(city) }
+            ]
+        );
+    };
+
+  return (
+    <ImageBackground source={image} resizeMode="cover" style={styles.container}>
+    <View style={styles.mainContainer}>
+    <Text style={styles.mainHeader}> New City Form </Text>
+        <View style={styles.inputContainer}>
+            <Text style={styles.labels}> Enter city name </Text>
+            <TextInput style={styles.inputStyle} onChangeText={e => {setCity({...city,city:e })}}/>
+        </View>
+        <View style={styles.inputContainer}>
+            <Text style={styles.labels}> Enter the country of the city </Text>
+            <TextInput style={styles.inputStyle} onChangeText={e => {setCity({...city,country:e })}}/>
+        </View>
+        <View style={styles.inputContainer}>
+            <Text style={styles.labels}> Enter a photo </Text>
+            <TextInput style={styles.inputStyle} keyboardType='url' onChangeText={e => {setCity({...city,photo:e })}} />
+        </View>
+        <View style={styles.inputContainer}>
+            <Text style={styles.labels}> Enter population </Text>
+            <TextInput style={styles.inputStyle} keyboardType='numeric' onChangeText={e => {setCity({...city,population:e })}} />
+        </View>
+        <View style={styles.inputContainer}>
+            <Text style={styles.labels}> Enter foundation </Text>
+            <TextInput style={styles.inputStyle} keyboardType='numeric' onChangeText={e => {setCity({...city,foundation:e })}} />
+        </View>
+        <View style={styles.btnForm}>
+            <Button title="SEND" onPress={handleSubmit} />
+        </View>
+    </View>
+    </ImageBackground>
+  )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    mainContainer:{
+        paddingHorizontal: 70,
+        paddingTop: 30,
+        marginBottom:160,
+        marginTop: 70,
+        backgroundColor: "#00000094",
+        flex:1,
+        alignItems:'center',
+        borderRadius:30,
+    },
+    mainHeader: {
+        fontSize: 40,
+        color: "#fff",
+        fontWeight: "bold",
+        paddingTop: 20,
+        paddingBottom: 15,
+    },
+    inputContainer:{
+        marginTop:30,
+        width: 400
+    },
+    labels:{
+        fontSize:20,
+        color: "#fff",
+        marginTop: 10,
+        marginBottom: 5,
+        lineHeight: 25,
+    },
+    inputStyle:{
+        borderWidth: 1,
+        borderColor: "#fff",
+        paddingHorizontal: 15,
+        paddingVertical: 7,
+        borderRadius: 30,
+        fontSize: 30,
+        color: "#fff"
+    },
+    btnForm: {
+        marginTop:60,
+        width:200,
+    }
+});
+
+export default NewCityScreen
