838f362c7b490fc7567bf7e7664322fc93858184
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-29 20:46:18 -0300

    Add New city screen

--- src/NavigationSrc/Drawer.js
+++ src/NavigationSrc/Drawer.js
@@ -4,6 +4,7 @@ import CitiesScreen from '../screens/CitiesScreen';
 import HomeScreen from '../screens/HomeScreen';
 import MyTineraryScreen from '../screens/MyTineraryScreen';
 import SignUpScreen from '../screens/SignUpScreen';
+import NewCityScreen from '../screens/NewCityScreen';
 
 const DrawerNav = createDrawerNavigator();
 
@@ -14,6 +15,7 @@ const Drawer = () => {
             <DrawerNav.Screen name='Cities' component={CitiesScreen}/>
             <DrawerNav.Screen name='MyTinerary' component={MyTineraryScreen}/>
             <DrawerNav.Screen name='Sign Up' component={SignUpScreen}/>
+            <DrawerNav.Screen name='New City' component={NewCityScreen}/>
         </DrawerNav.Navigator>
     );
 }
