18286af0dcf4880fd9923dc6c0aa4687fa692fd4
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-29 11:18:29 -0300

    Add MyTinerary

--- src/NavigationSrc/Drawer.js
+++ src/NavigationSrc/Drawer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import CitiesScreen from '../screens/CitiesScreen';
 import HomeScreen from '../screens/HomeScreen';
+import MyTineraryScreen from '../screens/MyTineraryScreen';
 
 const DrawerNav = createDrawerNavigator();
 
@@ -10,6 +11,7 @@ const Drawer = () => {
         <DrawerNav.Navigator>
             <DrawerNav.Screen name='Home' component={HomeScreen}/>
             <DrawerNav.Screen name='Cities' component={CitiesScreen}/>
+            <DrawerNav.Screen name='MyTinerary' component={MyTineraryScreen}/>
         </DrawerNav.Navigator>
     );
 }
