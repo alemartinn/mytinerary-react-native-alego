f48e906725178edc101436057ec9acce52f5549c
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-30 01:55:57 -0300

    added imagebackground to details

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import { useGetAllItinerariesCityQuery } from '../../features/itinerariesAPI';
 import DetailsCard from '../components/DetailsCard';
 import Itineraries from '../components/Itineraries';
@@ -7,13 +7,19 @@ import Itineraries from '../components/Itineraries';
 const DetailsScreen = ({ route, navigation }) => {
     
     const { itemId } = route.params;
+    const image = require('../../assets//backgroundHero.png')
 
     return (  
-        <View>
-            <DetailsCard itemId={itemId}/>
-            <Itineraries useGetAllItinerariesQuery={useGetAllItinerariesCityQuery} itemId={itemId}/>
-        </View>
+        <ImageBackground source={image} resizeMode="cover" style={styles.container}>
+                <DetailsCard itemId={itemId}/>
+                <Itineraries useGetAllItinerariesQuery={useGetAllItinerariesCityQuery} itemId={itemId}/>
+        </ImageBackground>
     );
 }
  
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+})
 export default DetailsScreen;
