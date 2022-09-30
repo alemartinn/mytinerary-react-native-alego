44febbe04d74ca3b6710d0cbfdef5ed733d1af8c
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-30 04:39:23 -0300

    add asyncstorage

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,13 +1,18 @@
 import FormSignUp from '../components/FormSignUp'
 import { ImageBackground, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function SignUpScreen({navigation}) {
-    const image = require('../../assets//backgroundHero.png')
+  const [userLog, setUserLog] = useState({})
+  AsyncStorage.getItem('user').then(value => setUserLog(value))
+  console.log(userLog);
+  const image = require('../../assets//backgroundHero.png')
   return (
     <ImageBackground source={image} resizeMode="cover" style={styles.container}>
       <ScrollView>
-        <FormSignUp role={'user'} navigation={navigation}/>
+        <FormSignUp role={userLog.role} navigation={navigation}/>
       </ScrollView>
     </ImageBackground>
   )
